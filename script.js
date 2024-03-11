e807d6484cfed554952c02eba30333ab9b55eb10
NNUTSI <89682164+NNUTSI@users.noreply.github.com>
2024-03-11 12:05:24 +0400

    home-work18

--- script.js
+++ script.js
@@ -2,14 +2,37 @@
 let inputElement = document.querySelector(".input-el");
 let add = document.querySelector(".button");
 let ulEl = document.querySelector(".ul-list");
+let clearAll = document.querySelector(".clear-all");
+let form = document.querySelector(".form");
 
-add.addEventListener("click", function () {
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //1. get input value
   let value = inputElement.value;
 
-  let li = documentElement("li");
+  // not to add clear string
+  if (value.trim() == "") {
+    return;
+  }
+  //2. value - li - ul
+  let li = document.createElement("li");
   li.textContent = value;
 
+  let deleteBtn = document.createElement("button");
+  //add Font Awesome icon class
+  deleteBtn.innerHTML = "<i class='fa-regular fa-trash-can icon'></i>";
+
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
   ulEl.appendChild(li);
 
-  inputElement.value = "";
+  //3. clear input
+  inputElement.value = " ";
+});
+//clear all
+clearAll.addEventListener("click", function () {
+  ulEl.innerHTML = " ";
 });
